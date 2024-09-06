9ae09248d11cfb4b3764b0c9d015dcc168a27705
tlelievre26 <jackrabbit5567@gmail.com>
2024-09-06 16:19:15 -0400

    Build is working locally

---
+++ electron/main.js
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from "electron"
+
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('./public/index.html')
+  }
+
+app.whenReady().then(() => {
+    createWindow()
+  })
