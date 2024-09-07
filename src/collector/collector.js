c8c2e9de489a082e7867e2b31229a57fc1b2694d
stephzhan <gadajiejie@gmail.com>
2024-09-07 11:40:25 -0400

    Error WRT require() conflicting with ES Module

--- src/collector/collector.js
+++
@@ -1,3 +0,0 @@
-//Just including this file as an example so that git tracks the directory structure
-
-//I don't actually remember what this collector folder was supposed to be for, or how its meant to be different from the api_recievers folder
