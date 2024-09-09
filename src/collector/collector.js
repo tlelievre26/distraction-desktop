02fc36628cc5aec1f1dbecc6df948a8a4f887263
stephzhan <gadajiejie@gmail.com>
2024-09-08 20:09:55 -0400

    winAPI testing

---
+++ src/collector/collector.js
@@ -0,0 +1,44 @@
+//Just including this file as an example so that git tracks the directory structure
+//I don't actually remember what this collector folder was supposed to be for, or how its meant to be different from the api_recievers folder
+const {
+     GetDefaultPrinter
+} = require ("win32-api/util")
+
+     const printerName = GetDefaultPrinter();
+     <h1>Hello World!</h1>
+     //<h1>printerName</h1>
+     //console.log(printerName);
+     //console.log("Hello World!");
+
+// ES6 syntax: import koffi from 'koffi';
+const koffi = require('koffi');
+// Load the shared library
+const lib = koffi.load('user32.dll');
+
+
+// Find functions
+let name = "Visual Studio Code";
+const GetAsyncKeyState = lib.func('__stdcall', 'GetAsyncKeyState', 'short', ['int']);
+const GetForegroundWindow = lib.func('__stdcall', 'GetForegroundWindow', 'void *', []);
+const GetWindowTextA = lib.func('int GetWindowTextA(void *, _Out_ char *, int)');
+let handle = GetForegroundWindow();
+console.log(handle);
+let string = ['\0'.repeat(200)];
+let txt = GetWindowTextA(handle, string, 200)
+//console.log(txt);
+console.log(string);
+let prev_string = [string, string];
+while(1)
+{
+  prev_string[1] = prev_string[0];
+  handle = GetForegroundWindow();
+  txt = GetWindowTextA(handle, string, 200);
+  prev_string[0] = string;
+  console.log(prev_string[0]);
+  //console.log(prev_string[1]);
+  if(prev_string[0] != prev_string[1])
+  {
+    console.log(txt);
+    console.log(prev_string[0]);
+  }
+}
