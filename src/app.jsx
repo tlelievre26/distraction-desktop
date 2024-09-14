fc42891fe5ec62e6cd53574ce0639867951b61cc
tlelievre26 <jackrabbit5567@gmail.com>
2024-09-14 17:02:32 -0400

    Implemented React

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+const React = require("react");
+
+const App = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/renderer.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  target: 'electron-renderer',
+};
