9acbeafa45b7fdb27cf1450be6129763c52f7565
tlelievre26 <jackrabbit5567@gmail.com>
2024-09-16 11:54:13 -0400

    Finished setting up ESLint config

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,11 @@
-const React = require('react');
-const ReactDOM = require('react-dom/client')
-const App   = require('./app')
+const React = require("react");
+const ReactDOM = require("react-dom/client");
 
+const App = require("./app");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
