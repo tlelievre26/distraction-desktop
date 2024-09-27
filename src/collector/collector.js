0819f0c8174ec39e8f86d61582a96ff5d69a7a15
stephzhan <gadajiejie@gmail.com>
2024-09-26 21:46:59 -0400

    added GetMessageA and WinEventProc to winapi-functions, added a memleak file, changed string4 name in collector.js to filepath

--- src/collector/collector.js
+++ src/collector/collector.js
@@ -16,26 +16,21 @@ const getCurrentAppName = () => {
 
   var ptr = [null];
   const MAXPATH = 260;
-  const string4 = Buffer.alloc(260);
+  const filepath = Buffer.alloc(260);
      
   GetWindowThreadProcessId(handle, ptr);
      
   let process = OpenProcess(0x1000 | 0x010, false, ptr[0]);
   // console.log("Process Id:", process) 
-     
-     
-  let query = GetModuleFileNameExA(process, null, string4, MAXPATH);
+  let query = GetModuleFileNameExA(process, null, filepath, MAXPATH);
   // console.log(query)
-     
-  const processName = string4.toString('utf8').replace(/\0/g, '');
+  const processName = filepath.toString('utf8').replace(/\0/g, '');
   //console.log(processName)
-     
-     
-  const fileSize = GetFileVersionInfoSizeA(string4, null);
+  const fileSize = GetFileVersionInfoSizeA(filepath, null);
   // console.log(fileSize)
   // console.log("Ive survived")
   const appInfo = Buffer.alloc(fileSize);
-  GetFileVersionInfoA(string4, 0, fileSize, appInfo);
+  GetFileVersionInfoA(filepath, 0, fileSize, appInfo);
   // console.log(appInfo.toString('utf8').replace(/\0/g, ''))
   const fullAppInfo = Buffer.alloc(100);
   const fullAppInfoSize = Buffer.alloc(100);
