9d3893c24b6d8dbcc7ada97ee6ab4bde996fd26a
tlelievre26 <jackrabbit5567@gmail.com>
2024-09-27 13:32:22 -0400

    Tweaking Chrome manifest

--- chrome_api/background.js
+++ chrome_api/background.js
@@ -1,13 +1,28 @@
-chrome.runtime.onStartup.addListener(() => {
-  chrome.runtime.sendNativeMessage('com.example.nativehost', { text: "Hello from Chrome extension" }, (response) => {
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Activating listener");
+  chrome.runtime.sendNativeMessage('com.distraction.nativehost', { text: "Hello from Chrome extension" }, (response) => {
     console.log('Received from native app:', response);
   });
 });
   
 // Listen for any messages from the popup or content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  chrome.runtime.sendNativeMessage('com.example.nativehost', request, (response) => {
+  chrome.runtime.sendNativeMessage('com.distraction.nativehost', request, (response) => {
     sendResponse(response);
   });
   return true;  // Keeps the message channel open for async response
 });
+
+const getCurrentTab = async (_activeInfo) => {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  console.log(tab);
+  return tab;
+};
+  
+chrome.tabs.onActivated.addListener(
+  getCurrentTab
+);
+  
+  
--- chrome_api/popup.js
+++ chrome_api/popup.js
@@ -1,42 +1,27 @@
 
+const tabData = () =>{
+  document.addEventListener('DOMContentLoaded', () => {
 
-// const tabData = () =>{
-//   document.addEventListener('DOMContentLoaded', () => {
-
-//     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     
 
-//       const currentTab = tabs[0];
+      const currentTab = tabs[0];
 
-//       const title = currentTab.title;
-//       const url = currentTab.url;
-//       const id = currentTab.id;
+      const title = currentTab.title;
+      const url = currentTab.url;
+      const id = currentTab.id;
 
     
-//       document.getElementById('tab-info').innerHTML = `
-//             <p><strong>Title:</strong> ${title}</p>
-//             <p><strong>URL:</strong> ${url}</p>
-//             <p><strong>ID:</strong> ${id}</p>
-//         `;
-//     });
-//   });
+      document.getElementById('tab-info').innerHTML = `
+            <p><strong>Title:</strong> ${title}</p>
+            <p><strong>URL:</strong> ${url}</p>
+            <p><strong>ID:</strong> ${id}</p>
+        `;
+    });
+  });
 
 
-// };
-
-
-// tabData();
-
-const getCurrentTab = async (_activeInfo) => {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
 };
 
 
-chrome.tabs.onActived.addListener(
-  getCurrentTab
-);
-
-
+tabData();
