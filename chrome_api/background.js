427aca26a38b2fed3604c4974b40ab0672273eaf
tlelievre26 <jackrabbit5567@gmail.com>
2024-09-27 13:32:22 -0400

    Tweaking Chrome manifest

--- chrome_api/background.js
+++ chrome_api/background.js
@@ -11,3 +11,45 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   });
   return true;  // Keeps the message channel open for async response
 });
+
+
+// const tabData = () =>{
+//   document.addEventListener('DOMContentLoaded', () => {
+
+//     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    
+
+//       const currentTab = tabs[0];
+
+//       const title = currentTab.title;
+//       const url = currentTab.url;
+//       const id = currentTab.id;
+
+    
+//       document.getElementById('tab-info').innerHTML = `
+//             <p><strong>Title:</strong> ${title}</p>
+//             <p><strong>URL:</strong> ${url}</p>
+//             <p><strong>ID:</strong> ${id}</p>
+//         `;
+//     });
+//   });
+
+
+// };
+
+
+// tabData();
+
+const getCurrentTab = async (_activeInfo) => {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+};
+  
+  
+chrome.tabs.onActived.addListener(
+  getCurrentTab
+);
+  
+  
