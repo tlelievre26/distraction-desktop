d7d67fa06ad8499c1cb692a257001ba6d1d784c5
tlelievre26 <jackrabbit5567@gmail.com>
2024-09-28 20:40:01 -0400

    Quick webPreferences config change

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, "preload.js"), // Optional if using preload script
       nodeIntegration: true,
+      nodeIntegrationInWorker: true,
       contextIsolation: false
     }
   });
