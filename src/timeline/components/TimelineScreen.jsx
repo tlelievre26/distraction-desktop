83a5b1277bd0c8c91452855cc986ec6765be0d94
Wilson Wong <wilsonwg640@gmail.com>
2024-10-14 13:47:18 -0400

    Added centering

--- src/timeline/components/TimelineScreen.jsx
+++ src/timeline/components/TimelineScreen.jsx
@@ -1,8 +1,11 @@
 const React = require("react");
 
 const Timeline = require("./timeline.jsx");
-// const BeginSessionButton = require("../../study-session/components/BeginSessionButton");
 
-const TimelineScreen = () => <div><Timeline/></div>;
+const TimelineScreen = () => (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <Timeline />
+  </div>
+);
 
 module.exports = TimelineScreen;
