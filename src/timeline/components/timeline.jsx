830f37e94fad28771eda50f08ef78cb8e2bc709d
Wilson Wong <wilsonwg640@gmail.com>
2024-10-14 13:47:18 -0400

    Added centering

--- src/timeline/components/timeline.jsx
+++ src/timeline/components/timeline.jsx
@@ -7,18 +7,17 @@ const { useState, useEffect } = React;
 const { useLocation } = require("react-router-dom");
 
 const TimeLine = () => {
-  // Define state for items and loading
   const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true); // Add loading state
+  const [loading, setLoading] = useState(true); 
   const sessionId = useLocation().state.sessionId;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const sessionData = await SpecificStudySessionProcessing(sessionId);
-        console.log("Session Data:", sessionData['Objects']); // Log the output of the function
+        console.log("Session Data:", sessionData['Objects']);
 
-        const newItems = []; // Initialize an empty array to store new items
+        const newItems = []; 
 
         // Loop through sessionData['Objects']
         for (let i = 0; i < sessionData['Objects'].length; i++) {
@@ -30,31 +29,31 @@ const TimeLine = () => {
             cardDetailedText: sessionData['Objects'][i]['_value'] // Use _value for title
           };
 
-          newItems.push(item); // Append the new dictionary to the list
+          newItems.push(item); 
         }
 
-        setItems(newItems); // Set the items state to the new list
-        setLoading(false); // Set loading to false after data is fetched
+        setItems(newItems); 
+        setLoading(false); 
 
       } catch (error) {
         console.error("Error fetching session data:", error);
-        setLoading(false); // Also set loading to false in case of error
+        setLoading(false);
       }
     };
 
     fetchData();
 
-  }, []); // Empty dependency array means it runs once when the component mounts
+  }, []); 
 
-  console.log("Items:", items); // Log the items variable
+  console.log("Items:", items);
 
-  // Conditional rendering based on loading state
+  
   if (loading) {
-    return <div>Loading...</div>; // Display loading message or spinner
+    return <div>Loading...</div>; 
   }
 
   return (
-    <div style={{ width: "500px", height: "400px" }}>
+    <div style={{ width: "500px", height: "500px" }}>
       <Chrono items={items} mode="HORIZONTAL" />
     </div>
   );
