7752a31b688adbafe29ced17f24de0bdaba98a66
stephzhan <78219467+stephzhan@users.noreply.github.com>
2024-10-15 19:00:33 -0400

    Delete src/collector/test.js

--- src/collector/test.js
+++
@@ -1,8 +0,0 @@
-const getCurrentAppName = require('./collector.js');
-const setCallback = require('./focus-event.js');
-
-while(1)
-{
-  //console.log("Return val: " + setCallback());
-  setCallback();
-}
