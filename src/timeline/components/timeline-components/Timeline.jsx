a42b2c3e71023ae4eeffa868aba19a67a337e6ad
stephzhan <gadajiejie@gmail.com>
2024-10-27 17:20:27 -0400

    timeline chunk dynamically changes with amount of time spent, only display chunks that exceed the minumum amount of time spent

--- src/timeline/components/timeline-components/Timeline.jsx
+++ src/timeline/components/timeline-components/Timeline.jsx
@@ -1,21 +1,48 @@
 /* eslint-disable no-unused-vars */
 // Only disable this temporarily so the linter stops yelling at us
+const { time } = require("console");
+
 const React = require("react");
 
 const { useSessionMetrics } = require("../SessionMetricsContext");
+const TimelineChunk = require('./TimelineChunk');
+const data = require("./two_hr_session.json");
 
 require("./TimelineStyles.css");
 
 const Timeline = () => {
   const { chunkSize, duration, sessionData } = useSessionMetrics();
-  const numChunks = Math.ceil(duration / (60 * chunkSize));
+  const numChunks = Math.ceil(120 * 60 / (60 * chunkSize));
   
-  const chunks = Array.from({ length: numChunks }, (_, index) => (
-    <div key={index} className="chunk" />
-  ));
+  const timeConversion = data.map(element => {
+    return new Date(element._time);
+  });
 
+  const timeSpent = timeConversion.map((element, index, arr) => {
+    if (index+1 < arr.length) {
+      const endTime = arr[index+1];
 
-  return <div className="timeline">{chunks}</div>;
+      const secondsStart = element.getUTCSeconds() + element.getUTCMinutes() * 60 + element.getUTCHours() * 3600;
+      const secondsEnd = endTime.getUTCSeconds() + endTime.getUTCMinutes() * 60 + endTime.getUTCHours() * 3600;
+    
+      return secondsEnd - secondsStart;
+    }
+    else
+    {
+      return 0;
+    }
+  });
+
+  return (
+    <div className="timeline"> {data.map((element, index) => (
+      <TimelineChunk
+        timeSpent={timeSpent[index]}
+        name={element._value}
+        timeChunk={chunkSize * 60}
+      />
+    ))}
+    </div>
+  );
 };
 
 module.exports = Timeline;
