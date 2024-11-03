358024c2e324980595923a7ce61b44cf2cf3065e
tlelievre26 <jackrabbit5567@gmail.com>
2024-11-03 16:40:35 -0500

    Bugfix

--- src/study-session/session-control.js
+++ src/study-session/session-control.js
@@ -7,8 +7,7 @@ const { currentTime } = require("@influxdata/influxdb-client");
 
 const log = require('../util/logger');
 const { appData, insertStudySessionData } = require("../api_recievers/influxqueries");
-
-
+const { stopInfluxDb } = require("../api_recievers/start-influx-db");
 let winApiThread;
 let sessionId;
 let wss;
